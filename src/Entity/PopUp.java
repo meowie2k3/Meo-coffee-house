4a9b01cbdb49584cb79e3f1da54021ef41188a94
Huy Dao <daominhhuy.9a1@gmail.com>
2022-12-27 21:18:47 +0700

    slight update to popup

--- src/Entity/PopUp.java
+++ src/Entity/PopUp.java
@@ -1,5 +1,37 @@
 package Entity;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import Map.*;
+
 public class PopUp {
+    private int x;
+    private int y;
+    private BufferedImage icon;
+    private BufferedImage whitePad;
+
+    private final int spacing = 10;
     
+    public PopUp(int x, int y, String address){
+        this.x = x;
+        this.y = y;
+        try{
+            icon = ImageIO.read(getClass().getResourceAsStream(address));
+            whitePad = ImageIO.read(getClass().getResource("/UI/whitePad.png"));
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void draw(Graphics2D g){
+        
+    }
+
+
 }
