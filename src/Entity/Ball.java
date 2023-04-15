314af09c211f530ed4ebf5d45a5f81e111bd1d96
HngNg <tanhung09brvt@outlook.com.vn>
2023-04-15 23:40:19 +0700

    setters & getters were added

--- src/Entity/Ball.java
+++ src/Entity/Ball.java
@@ -11,17 +11,38 @@ public class Ball {
     private int x;
     private int y;
     private BufferedImage img;
-    private boolean isClicked;
+    private boolean isClicked_;
     private int level = 0;
+    private int colorCode;
 
     private String[] color_address = {
 
     }; 
-    
 
-    public Ball(int color_id, int x_pos, int y_pos){
+    private int getx() {
+        return x;
+    } 
+
+    private int gety() {
+        return y;
+    }
+
+    private boolean isClicked() {
+        return isClicked_;
+    } 
+
+    private int getLevel() {
+        return level;
+    }
+
+    private int getColor() {
+        return colorCode;
+    }
+
+    public Ball(int x_pos, int y_pos, int color_id, int lvl){
         try{
             img = ImageIO.read(getClass().getResourceAsStream(color_address[color_id-1]));
+            
             x = x_pos; y = y_pos;
             level = 0;
             
@@ -56,8 +77,6 @@ public class Ball {
             }
 
         }
-            
-
     }
 
     public void draw(Graphics2D g){
