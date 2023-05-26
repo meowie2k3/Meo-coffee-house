a0510be5cb199703170fcc369bade1350d59ac24
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2023-05-26 14:29:10 +0700

    half AStar (BFS oriented)

--- src/Map/AStarCalculation.java
+++ src/Map/AStarCalculation.java
@@ -1,89 +1,86 @@
 package Map;
 
 import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.Stack;
+import java.util.Comparator;
 
-class Pair{
+class Pair {
     int first, second;
-    Pair(){}
-    Pair(int first, int second){
+
+    Pair() {
+    }
+
+    Pair(int first, int second) {
         this.first = first;
         this.second = second;
     }
 }
 
-class pPair{
+class pPair {
     double first;
     Pair second;
-    pPair(){}
-    pPair(double first, Pair second){
+
+    pPair() {
+    }
+
+    pPair(double first, Pair second) {
         this.first = first;
         this.second = second;
     }
 }
 
-class cell{
-    //Row and Column index of its parent
-    //Note that 0 <= i <= ROW-1 & 0 <= j <= COL-1
+class cell {
+    // Row and Column index of its parent
+    // Note that 0 <= i <= ROW-1 & 0 <= j <= COL-1
     int parent_i, parent_j;
-    //f = g + h
+    // f = g + h
     double f, g, h;
 }
 
 public class AStarCalculation {
 
-    //typedef pair<int, int> Pair;
-    //typedef pair<double, pair<int, int>> pPair;
-
     public static final int ROW = 9;
     public static final int COL = 9;
 
-    
-
-    static void memset(boolean[][] arr, boolean value, int size){
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
+    private static void memset(boolean[][] arr, boolean value, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 arr[i][j] = value;
             }
         }
     }
 
-    static boolean isValid(int row, int col){
+    private static boolean isValid(int row, int col) {
         return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL);
     }
-    
-    static boolean isUnBlocked(int grid[][], int row, int col)
-    {
-    // Returns true if the cell is not blocked else false
-    if (grid[row][col] == 1)
-        return (true);
-    else
-        return (false);
+
+    private static boolean isUnBlocked(int[][] grid, int row, int col) {
+        return grid[row][col] == 1;
     }
 
-    static boolean isDestination(int row, int col, Pair dest)
-    {
+    private static boolean isDestination(int row, int col, Pair dest) {
         if (row == dest.first && col == dest.second)
             return (true);
         else
             return (false);
     }
 
-    static double calculateHValue(int row, int col, Pair dest){
-        //Manhattan distance
-        //only 4 directions
-        return ((double)Math.abs(row - dest.first) + Math.abs(col - dest.second));
+    private static double calculateHValue(int row, int col, Pair dest) {
+        // Manhattan distance
+        // only 4 directions
+        return ((double) Math.abs(row - dest.first) + Math.abs(col - dest.second));
     }
 
-    static void tracePath(cell[][] cellDetails, Pair dest){
+    private static void tracePath(cell[][] cellDetails, Pair dest) {
         System.out.println("\nThe Path is ");
         int row = dest.first;
         int col = dest.second;
 
         Stack<Pair> Path = new Stack<Pair>();
 
-        while(!(cellDetails[row][col].parent_i == row && cellDetails[row][col].parent_j == col)){
+        while (!(cellDetails[row][col].parent_i == row && cellDetails[row][col].parent_j == col)) {
             Path.push(new Pair(row, col));
             int temp_row = cellDetails[row][col].parent_i;
             int temp_col = cellDetails[row][col].parent_j;
@@ -92,7 +89,7 @@ public class AStarCalculation {
         }
 
         Path.push(new Pair(row, col));
-        while(!Path.empty()){
+        while (!Path.empty()) {
             Pair p = Path.peek();
             Path.pop();
             System.out.print("-> (" + p.first + ", " + p.second + ") ");
@@ -100,40 +97,40 @@ public class AStarCalculation {
         return;
     }
 
-    static void AStarSearch(int[][] grid, Pair src, Pair dest){
-        
-        //source valid check
-        if(!isValid(src.first, src.second)){
+    static void AStarSearch(int[][] grid, Pair src, Pair dest) {
+
+        // source valid check
+        if (!isValid(src.first, src.second)) {
             System.out.println("Source is invalid");
             return;
         }
 
-        //destination valid check
-        if(!isValid(dest.first, dest.second)){
+        // destination valid check
+        if (!isValid(dest.first, dest.second)) {
             System.out.println("Destination is invalid");
             return;
         }
 
-        //destination blocked check
-        if(!isUnBlocked(grid, dest.first, dest.second)){
+        // destination blocked check
+        if (!isUnBlocked(grid, dest.first, dest.second)) {
             System.out.println("Destination is blocked");
             return;
         }
 
-        //Create a closed list and initialise it to false which
-        //means that no cell has been included yet
-        //This closed list is implemented as a boolean 2D array
+        // Create a closed list and initialise it to false which
+        // means that no cell has been included yet
+        // This closed list is implemented as a boolean 2D array
         boolean[][] closedList = new boolean[ROW][COL];
         memset(closedList, false, ROW);
 
-        //Declare a 2D array of structure to hold the details
-        //of that cell
+        // Declare a 2D array of structure to hold the details
+        // of that cell
         cell[][] cellDetails = new cell[ROW][COL];
 
         int i, j;
 
-        for(i = 0; i < ROW; i++){
-            for(j = 0; j < COL; j++){
+        for (i = 0; i < ROW; i++) {
+            for (j = 0; j < COL; j++) {
                 cellDetails[i][j] = new cell();
                 cellDetails[i][j].f = Double.MAX_VALUE;
                 cellDetails[i][j].g = Double.MAX_VALUE;
@@ -143,7 +140,7 @@ public class AStarCalculation {
             }
         }
 
-        //Initialising the parameters of the starting node
+        // Initialising the parameters of the starting node
         i = src.first;
         j = src.second;
         cellDetails[i][j].f = 0.0;
@@ -152,137 +149,82 @@ public class AStarCalculation {
         cellDetails[i][j].parent_i = i;
         cellDetails[i][j].parent_j = j;
 
-        /*
-        Create an open list having information as-
-        <f, <i, j>>
-        where f = g + h,
-        and i, j are the row and column index of that cell
-        Note that 0 <= i <= ROW-1 & 0 <= j <= COL-1
-        This open list is implenented as a set of pair of pair.*/
         Set<pPair> openList = new java.util.HashSet<pPair>();
 
-        //Put the starting cell on the open list and set its
-        //f as 0
+        // Put the starting cell on the open list and set its
+        // f as 0
         openList.add(new pPair(0.0, new Pair(i, j)));
 
-        //We set this boolean value as false as initially
-        //the destination is not reached.
+        // We set this boolean value as false as initially
+        // the destination is not reached.
         boolean foundDest = false;
 
-        while(!openList.isEmpty()){
+        while (!openList.isEmpty()) {
             pPair p = openList.iterator().next();
 
-            //Remove this vertex from the open list
+            // Remove this vertex from the open list
             openList.remove(p);
 
-            //Add this vertex to the closed list
+            // Add this vertex to the closed list
             i = p.second.first;
             j = p.second.second;
             closedList[i][j] = true;
 
-            /*
-            Generating all the 4 successor of this cell
+            ArrayList<Pair> successors = new ArrayList<Pair>();
+            if (isValid(i - 1, j)) {
+                successors.add(new Pair(i - 1, j));
+            }
+            if (isValid(i + 1, j)) {
+                successors.add(new Pair(i + 1, j));
+            }
+            if (isValid(i, j + 1)) {
+                successors.add(new Pair(i, j + 1));
+            }
+            if (isValid(i, j - 1)) {
+                successors.add(new Pair(i, j - 1));
+            }
 
-                    N
-                    |
-                    |
-            W ----Cell---- E
-                    |
-                    |
-                    S
-            Cell-->Popped Cell (i, j)
-            N --> North, S --> South, E --> East, W --> West
-            */
+            successors.sort(Comparator.comparingDouble(pair -> calculateHValue(pair.first, pair.second, dest)));
 
-            //To store the 'g', 'h' and 'f' of the 4 successors
+            // To store the 'g', 'h' and 'f' of the 4 successors
             double gNew, hNew, fNew;
 
-            //----------- 1st Successor (North) ------------
+            // Looping through all the successors
+            for (Pair successor : successors) {
+                int row = successor.first;
+                int col = successor.second;
 
-            //Only process this cell if this is a valid one
-            if(isValid(i-1, j) == true){
+                if (isValid(row, col) && closedList[row][col] == false && isUnBlocked(grid, row, col)) {
+                    if (isDestination(row, col, dest)) {
+                        // Set the Parent of the destination cell
+                        cellDetails[row][col].parent_i = i;
+                        cellDetails[row][col].parent_j = j;
+                        System.out.println("The destination cell is found\n");
+                        tracePath(cellDetails, dest);
+                        foundDest = true;
+                        return;
+                    } else {
+                        gNew = cellDetails[i][j].g + 1.0;
+                        hNew = calculateHValue(row, col, dest);
+                        fNew = gNew + hNew;
 
-                if(isDestination(i-1, j, dest)==true){
-                    //Set the Parent of the destination cell
-                    cellDetails[i-1][j].parent_i = i;
-                    cellDetails[i-1][j].parent_j = j;
-                    System.out.println("The destination cell is found\n");
-                    tracePath(cellDetails, dest);
-                    foundDest = true;
-                    return;
-                }
-                //If the successor is already on the closed
-                //or if it is blocked, then ignore it.
-                //else do the following
-                else if(closedList[i-1][j] == false && 
-                        isUnBlocked(grid, i-1, j) == true){
-                    gNew = cellDetails[i][j].g + 1.0;
-                    hNew = calculateHValue(i-1, j, dest);
-                    fNew = gNew + hNew;
+                        if (cellDetails[row][col].f == Double.MAX_VALUE || cellDetails[row][col].f > fNew) {
+                            openList.add(new pPair(fNew, new Pair(row, col)));
 
-                    if(cellDetails[i-1][j].f == Double.MAX_VALUE ||
-                            cellDetails[i-1][j].f > fNew){
-                        openList.add(new pPair(fNew, new Pair(i-1, j)));
-
-                        //Update the details of this cell
-                        cellDetails[i-1][j].f = fNew;
-                        cellDetails[i-1][j].g = gNew;
-                        cellDetails[i-1][j].h = hNew;
-                        cellDetails[i-1][j].parent_i = i;
-                        cellDetails[i-1][j].parent_j = j;
+                            // Update the details of this cell
+                            cellDetails[row][col].f = fNew;
+                            cellDetails[row][col].g = gNew;
+                            cellDetails[row][col].h = hNew;
+                            cellDetails[row][col].parent_i = i;
+                            cellDetails[row][col].parent_j = j;
+                        }
                     }
                 }
-            }
-
-            //----------- 2nd Successor (South) ------------
-
-            //Only process this cell if this is a valid one
-            if(isValid(i+1, j) == true){
-                //If the destination cell is the same as the
-                //current successor
-                if(isDestination(i+1, j, dest) == true){
-                    //Set the Parent of the destination cell
-                    cellDetails[i+1][j].parent_i = i;
-                    cellDetails[i+1][j].parent_j = j;
-                    System.out.println("The destination cell is found\n");
-                    tracePath(cellDetails, dest);
-                    foundDest = true;
-                    return;
-                }
-                else if(closedList[i+1][j] == false &&
-                        isUnBlocked(grid, i + 1, j) == true){
-                    gNew = cellDetails[i][j].g + 1.0;
-                    hNew = calculateHValue(i+1, j, dest);
-                    fNew = gNew + hNew;
-
-                    if(cellDetails[i + 1][j].f == Double.MAX_VALUE ||
-                        cellDetails[i + 1][j].f > fNew){
-                        openList.add(new pPair(fNew, new Pair(i+1, j)));
-
-                        //Update the details of this cell
-                        cellDetails[i+1][j].f = fNew;
-                        cellDetails[i+1][j].g = gNew;
-                        cellDetails[i+1][j].h = hNew;
-                        cellDetails[i+1][j].parent_i = i;
-                        cellDetails[i+1][j].parent_j = j;
-
-                    }
-                }
-            }
-
-            //----------- 3rd Successor (East) ------------
-
-            if(isValid(i, j + 1) == true){
-                if(isDestination(i, j + 1, dest) == true){
-                    cellDetails[i][j + 1].parent_i = i;
-                    cellDetails[i][j + 1].parent_j = j;
-                    System.out.println("The destination cell is found\n");
-                    tracePath(cellDetails, dest);
-                    foundDest = true;
-                    return;
-                }
-                
             }
         }
+        if (foundDest == false) {
+            System.out.println("Failed to find the Destination Cell\n");
+        }
+        return;
     }
 }
